60e11402a8de986a14b2bb62ed0e772f62497c4f
Anthony Tornetta <cornchipgonecodin@gmail.com>
2024-08-27 19:53:51 -0400

    Removed key

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,11 +10,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_key_pair" "my_key_pair" {
-  key_name = "my_key_pair"
-  public_key = file("~/.ssh/id_ras.pub")
-}
-
 resource "aws_security_group" "allow_ssh_http" {
   name = "allow_ssh_http"
   description = "Allows SSH and HTTP(S) traffic"
@@ -52,7 +47,7 @@ resource "aws_instance" "my_ec2_instance" {
   ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
 
-  key_name = aws_key_pair.my_key_pair.key_name
+  # key_name = aws_key_pair.my_key_pair.key_name
   security_groups = aws_security_group.allow_ssh_http.name
 
   tags = {
