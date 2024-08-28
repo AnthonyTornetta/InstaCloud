f9a21a048c66bac6c2c84dfa4e01994bce5a8c4d
Anthony Tornetta <cornchipgonecodin@gmail.com>
2024-08-27 21:55:30 -0400

    Formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,44 +11,44 @@ provider "aws" {
 }
 
 resource "aws_security_group" "allow_ssh_http" {
-  name = "allow_ssh_http"
+  name        = "allow_ssh_http"
   description = "Allows SSH and HTTP(S) traffic"
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "my_ec2_instance" {
-  ami = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-066784287e358dad1"
   instance_type = "t2.micro"
 
   # key_name = aws_key_pair.my_key_pair.key_name
-  security_groups = [ aws_security_group.allow_ssh_http.name ]
+  security_groups = [aws_security_group.allow_ssh_http.name]
 
   tags = {
     Name = "MyEC2Instance"
