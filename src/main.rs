681ef4d77c8b2dea951e34b24f26f139adeec272
Anthony Tornetta <cornchipgonecodin@gmail.com>
2024-08-29 19:48:55 -0400

    Stopped deleting the entire terraform state

--- src/main.rs
+++ src/main.rs
@@ -28,12 +28,19 @@ fn main() -> anyhow::Result<()> {
 
     let cloud_config = CloudConfig::new(&vars, cloud_config, base_path)?;
 
+    // Removes all old generated files w/out deleting the current terraform state.
+    let delete_extensions = ["zip", "tf"];
     for entry in WalkDir::new("terraform/generated") {
         let Ok(entry) = entry else {
             continue;
         };
 
-        if entry.path().ends_with(".tf") {
+        if entry
+            .path()
+            .extension()
+            .map(|x| delete_extensions.contains(&x.to_str().unwrap_or("")))
+            .unwrap_or(false)
+        {
             fs::remove_file(entry.path()).expect("Unable to remove old tf file.");
         }
     }
